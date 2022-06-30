bfae5afd212cafbabc08cea6433f1abaf89235df
Sandesh <psandesh01.sp@gmail.com>
2022-06-30 20:35:46 +0530

    Added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is:
